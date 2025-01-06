6710354fcb8233065c3fc713663bb47fa8e532db
aroojshahzadi39 <aroojshahzadi1098@gmail.com>
2025-01-06 10:18:45 +0100

    Alles aktualisiert

--- src/main/java/at/ac/fhcampuswien/hangman/HangmanController.java
+++ src/main/java/at/ac/fhcampuswien/hangman/HangmanController.java
@@ -1,14 +1,49 @@
 package at.ac.fhcampuswien.hangman;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class HangmanController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HangmanController implements Initializable {
+        @FXML
+        private ChoiceBox<String> categoryButton;
+
+        private WordProvider wordProvider;
+
+        private GameLogic gameLogic;
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+            wordProvider = new WordProvider();
+
+
+            categoryButton.getItems().addAll(wordProvider.getCategories());
+        }
+
+        public void onPlayButtonClick(){
+            String selectedCategory = categoryButton.getValue();
+            if (selectedCategory != null){
+                try {
+                    Stage currentStage = (Stage) categoryButton.getScene().getWindow();
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("hangman-view.fxml"));
+                    AnchorPane root = loader.load();
+                    Scene scene = new Scene(root);
+
+                    currentStage.setScene(scene);
+                    currentStage.close();
+                    currentStage.show();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
-}
