d2c175e1678df9c513243db48ce5764c19e8bc3d
ZeynepSerttas <zeynep.serttas@stud.fh-campuswien.ac.at>
2025-01-15 22:47:24 +0100

    Wordprovider angepasst

--- src/main/java/at/ac/fhcampuswien/hangman/WordProvider.java
+++ src/main/java/at/ac/fhcampuswien/hangman/WordProvider.java
@@ -8,11 +8,11 @@ public class WordProvider {
         public WordProvider() {
             wordCategories = new HashMap<>();
 
-            wordCategories.put("Animals", Arrays.asList("Dog","Cat","Elephant","Lion","Tiger","Monkey","Giraffe","Donkey","Goat","Goose","Horse","Sheep","Turtle","Zebra","Dolphin","Kangaroo","Snake","Eagle","Whale","Octopus","Butterfly","Spider","Mouse","Rabbit","Squirrel"));
-            wordCategories.put("Countries", Arrays.asList("Austria","Germany","France","Spain","Denmark","England","Canada","Finland","Sweden","Norway","Italy","Netherlands","Greece","India","Ireland","Iceland","Japan","China","Croatia","Mexico","Poland","Russia","Switzerland","Hungary","Portugal","Belgium","Australia"));
-            wordCategories.put("Sports", Arrays.asList("Basketball","Football","Tennis","Badminton","Baseball","Bowling","Boxing","Climbing","Cycling","Golf","Hockey","Handball","Hiking","Skiing","Surfing","Swimming","Volleyball","Diving","Darts","Running"));
-            wordCategories.put("Colours", Arrays.asList("Black","Blue","Green","Yellow","Orange","Purple","Pink","Brown","Grey","Turquoise","Khaki","Azure","Cyan","Magenta","Beige"));
-            wordCategories.put("Fruits", Arrays.asList("Pineapple","Apple","Banana","Cherry","Orange","Pear","Blackberry","Blueberry","Strawberry","Raspberry","Kiwi","Coconut","Mango","Watermelon","Peach","Grape","Lemon","Plum","Lychee","Dragonfruit","Pomegranate","Papaya"));
+            wordCategories.put("Animals", Arrays.asList("Okapi","Pangolin","Elephant","Gecko","Axolotl","Monkey","Giraffe","Donkey","Goat","Goose","Horse","Sheep","Turtle","Aardvark","Dolphin","Kangaroo","Snake","Eagle","Whale","Octopus","Butterfly","Spider","Mouse","Rabbit","Squirrel"));
+            wordCategories.put("Countries", Arrays.asList("Austria","Bhutan","Mozambique","Kyrgyzstan","Laos","Denmark","Somalia","Paraguay","Finland","Thailand","Norway","Kasachstan","Netherlands","Myanmar","Jemen","Ireland","Iceland","Japan","Pakistan","Croatia","Mexico","Poland","Vietnam","Switzerland","Hungary","Usbekistan","Belgium","Australia"));
+            wordCategories.put("Sports", Arrays.asList("Curling","Taekwondo","Parkour","Badminton","Baseball","Bowling","Boxing","Climbing","Cycling","Golf","Hockey","Handball","Hiking","Skiing","Surfing","Swimming","Judo","Diving","Darts","Running"));
+            wordCategories.put("Colours", Arrays.asList("Black","Blue","Mauve","Yellow","Orange","Purple","Pink","Brown","Grey","Turquoise","Khaki","Azure","Cyan","Magenta","Beige"));
+            wordCategories.put("Fruits", Arrays.asList("Pineapple","Durian","Banana","Cherry","Orange","Pear","Blackberry","Blueberry","Strawberry","Raspberry","Kiwi","Coconut","Mango","Watermelon","Peach","Grape","Lemon","Plum","Lychee","Dragonfruit","Pomegranate","Papaya"));
         }
 
         public List<String> getCategories(){
