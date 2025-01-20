9d178aae42426a033612f8553906e3ee2ffde5d3
aroojshahzadi39 <aroojshahzadi1098@gmail.com>
2025-01-20 21:17:19 +0100

    GameOverController aktualisiert

--- src/main/java/at/ac/fhcampuswien/hangman/GameOverController.java
+++ src/main/java/at/ac/fhcampuswien/hangman/GameOverController.java
@@ -29,11 +29,16 @@ public class GameOverController {
     @FXML
     public void onQuitButtonClick(ActionEvent event) {
         try {
+
             FXMLLoader loader = new FXMLLoader(getClass().getResource("start-view.fxml"));
             AnchorPane root = loader.load();
+
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+
             stage.setScene(new Scene(root));
             stage.show();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
